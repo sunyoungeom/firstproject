bc67afb661cf6967adb6fd85726fddfd7772ad13
sun <sunyoung.eom@outlook.com>
2023-12-27 14:36:08 +0900

    추가 메시지 출력 작성

--- 231227_vcs/src/Main.java
+++ 231227_vcs/src/Main.java
@@ -7,8 +7,15 @@
 // 각자의 개인 컴퓨터에 자원(파일) 기록
 // local repository 생성
 
+// commit -> 기록 생성
+// history -> 기록 열람
+// checkout -> 원하는 시점으로 이동
+
+// github -> remote repository
+// push -> 원격 저장소로 업로드
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("헬로 월드");
+		System.out.println("추가 문장 작성");
 	}
 }
