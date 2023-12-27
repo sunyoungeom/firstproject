7bcab75dcbcc3366661d0f10338d0cb53a61eaf2
sun <sunyoung.eom@outlook.com>
2023-12-27 12:49:34 +0900

    프로젝트를 생성하고 Main 클래스를 작성하였습니다.

---
+++ 231227_vcs/src/Main.java
@@ -0,0 +1,12 @@
+// VCS : version control system
+// svn, git
+
+// 다운로드 -> 환경변수 설정 -> CLI command 입력 제어
+
+// eclipse 내부에 git GUI 플러그인
+// 각자의 개인 컴퓨터에 자원(파일) 기록
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
